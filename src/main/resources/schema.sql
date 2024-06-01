bc5c1dd254ce976a6ad1128d05acc0580bc1f978
xy-Summer-Sky <summer-xy2022@outlook.com>
2024-06-01 20:23:52 +0800

    小调整脚本

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -45,11 +45,13 @@ CREATE TABLE IF NOT EXISTS responses (
 );
 create table if not exists surveys_state
 (
-    survey_id     bigint                                    not null,
+    survey_id     bigint       not null,
     id            int auto_increment
         primary key,
-    receiveNumber int                                       not null,
-    state         enum ('已发布', '未发布', '结束', '暂停') not null,
+    receiveNumber int          not null,
+    state         varchar(255) null,
+    constraint surveys_state_pk
+        unique (survey_id),
     constraint survey_id
         foreign key (survey_id) references surveys (id)
 );
