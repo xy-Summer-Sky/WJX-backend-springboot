a3815373ba3eade209388bc08bef668a29cbd073
hcp <3032546036@qq.com>
2024-06-05 20:22:20 +0800

    调试

--- src/main/java/com/example/backend/service/CustomUserDetailsService.java
+++ src/main/java/com/example/backend/service/CustomUserDetailsService.java
@@ -21,7 +21,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         // System.out.println("loadUserByUsername is called with email: " + email);
         User user;
-        if (email.contains("@") && email.contains(".com"))
+        if (email.contains("@") &&  email.contains(".com"))
             user = userRepository.findByEmail(email);
         else
             user = userRepository.findByName(email);
